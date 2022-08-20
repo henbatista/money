769868ee2da8e994eef0e155f7c6d78d8c146aa6
Henrique Batista <wellington.h.batista@hotmail.com>
2022-08-19 23:08:20 -0300

    project Money transactions

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { SummaryCard, SummaryContainer } from "./styles";
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Input</span>
+          <ArrowCircleUp size={32} color="#00B37E" />
+        </header>
+        <strong>$ 17,400.00</strong>
+      </ SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Inputs</span>
+          <ArrowCircleDown size={32} color="#F75A68" />
+        </header>
+        <strong>$ 17,400.00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#FFF" />
+        </header>
+        <strong>$ 17,400.00</strong>
+      </SummaryCard>
+
+    </SummaryContainer>
+  )
+}
